845e69f6b6620685c0bd95e1f9d38712892f25d3
Piotr Podusowski <podusowski@gmail.com>
2017-07-27 16:52:00 +0200

    new type of crack

---
+++ crack_using_dictionary_permutation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from itertools import product, islice
+import vigenere
+from crack5 import Dictionary
+import crack5
+import utils
+
+
+s = 'Ccoheal ieu w qwu tcb'.lower()
+dictionary = Dictionary('dictionary.txt', {len(w) for w in s.split()})
+
+
+def words(length):
+    return {w for w in dictionary.words if len(w) == length}
+
+
+l = [words(len(w)) for w in s.split()]
+
+
+@utils.log_nth_call(10000)
+def check_key(key):
+    decrypted = ''.join(x)
+    key = vigenere.find_shortest_key(s, decrypted)
+
+    if len(key) == len(decrypted):
+        return
+
+    print('{} {}'.format(decrypted, key))
+
+    if crack5.check_online(key):
+        print("legan! {}".format(key))
+
+
+for x in product(*l):
+    decrypted = ''.join(x)
+    key = vigenere.find_shortest_key(s, decrypted)
+
+    check_key(key)
